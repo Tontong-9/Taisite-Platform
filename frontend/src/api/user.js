8b2d792ac7232308600fdcd0e2ffcab91c316a19
yter <tontong_9@163.com>
2023-06-29 22:55:04 +0800

    # add user.js

---
+++ frontend/src/api/user.js
@@ -0,0 +1,19 @@
+import requests from "@/utils/request"
+
+export function login(params, header){
+    return request({
+        url:'/api/login',
+        headers:header,
+        method: 'POST',
+        data: params,
+    })
+}
+
+export function logout(params, header){
+    return request({
+        url: '/api/logout',
+        headers: header,
+        method: 'POST',
+        data: params,
+    })
+}
